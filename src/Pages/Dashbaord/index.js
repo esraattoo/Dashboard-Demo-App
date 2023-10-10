b7fe5087be7a437d482f20d86383bd8c655369a0
esraattoo <147481246+esraattoo@users.noreply.github.com>
2023-10-10 11:09:12 +0300

    init

---
+++ src/Pages/Dashbaord/index.js
@@ -0,0 +1,142 @@
+import {
+  UserOutlined,
+} from "@ant-design/icons";
+import { Card, Space, Statistic, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getCustomers, getRevenue } from "../../API";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function Dashboard() {
+  const [customers, setCustomers] = useState(0);
+
+  useEffect(() => {
+    getCustomers().then((res) => {
+      setCustomers(res.total);
+    });
+  }, []);
+
+  return (
+    <Space size={20} direction="vertical">
+      <Typography.Title level={4}>Dashboard</Typography.Title>
+      <Space direction="horizontal">
+        <DashboardCard
+          icon={
+            <UserOutlined
+              style={{
+                color: "purple",
+                backgroundColor: "rgba(0,255,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title={"Customer"}
+          value={customers}
+        />
+        <DashboardCard
+          icon={
+            <UserOutlined
+              style={{
+                color: "purple",
+                backgroundColor: "rgba(0,255,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title={"Customer"}
+          value={customers}
+        />
+      </Space>
+      <Space>
+        <DashboardChart />
+      </Space>
+      <DashboardChart />
+
+    </Space>
+  );
+}
+
+function DashboardCard({ title, value, icon }) {
+  return (
+    <Card>
+      <Space direction="horizontal">
+        {icon}
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  );
+}
+
+// DashboardChart
+function DashboardChart() {
+  const [reveneuData, setReveneuData] = useState({
+    labels: [],
+    datasets: [],
+  });
+
+  useEffect(() => {
+    getRevenue().then((res) => {
+      const labels = res.carts.map((cart) => {
+        return `User test ${cart.userId}`;
+      });
+      const data = res.carts.map((cart) => {
+        return cart.discountedTotal;
+      });
+
+      const dataSource = {
+        labels,
+        datasets: [
+          {
+            label: "Revenue",
+            data: data,
+            backgroundColor: "rgba(255, 0, 0, 1)",
+          },
+        ],
+      };
+
+      setReveneuData(dataSource);
+    });
+  }, []);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Order Revenue",
+      },
+    },
+  };
+
+  return (
+    <Card style={{ width: 500, height: 250 }}>
+      <Bar options={options} data={reveneuData} />
+    </Card>
+  );
+}
+export default Dashboard;
